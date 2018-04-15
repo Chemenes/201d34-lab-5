cac9e2b0696fe7d034de065f5454bc812c49388b
Christopher <conrad317@gmail.com>
2018-04-15 00:25:47 -0700

    Problem 5 complete

--- app.js
+++ app.js
@@ -54,21 +54,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-  var threeNumber = sum(a, sum(b,c)[0])[0];
+// function sumAndMultiply(a, b, c) { //eslint-disable-line
+//   var threeNumber = sum(a, sum(b,c)[0])[0];
 
-  var oneSum = sum(a, b)[0]; 
-  var twoSum = sum(c, oneSum)[0];
+//   var oneSum = sum(a, b)[0]; 
+//   var twoSum = sum(c, oneSum)[0];
 
-  var multiplyNum = multiply(a, multiply(b,c)[0])[0];
+//   var multiplyNum = multiply(a, multiply(b,c)[0])[0];
 
-  var returnOne = a + ' and ' + b + ' and ' + c + ' sum to ' + threeNumber + '.';
-  var returnTwo = 'The product of' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyNum + '.';
+//   var returnOne = a + ' and ' + b + ' and ' + c + ' sum to ' + threeNumber + '.';
+//   var returnTwo = 'The product of' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyNum + '.';
  
-  // return [threeNumber, multiplyNum, oneSum, twoSum];
-}
+//   // return [threeNumber, multiplyNum, oneSum, twoSum];
+// }
 
-// // Here is the test for sumAndMultiply(); uncomment it to run it
+// // // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -95,7 +95,7 @@ return [theSum, message];
 
 // Here is the test for sumArray(); uncomment it to run it
 
-sumArray(testArray);
+// sumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -110,12 +110,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+function multiplyArray(multiArray){ //eslint-disable-line
+  var theSum = multiply(multiArray[0], multiply(multiArray[1], multiArray[2])[0])[0];
+  
+  // var multiOne = multiply(multiArray[0], multiArray[1]);
+  // multiOne = multiOne[0];
+  // var multiTwo = multiply(multiplyArray[2], multiOne);
+  // multiTwo = multiTwo[0];
+ 
+  var message = ' The numbers ' + multiArray + ' have a product of ' + theSum + '.';
+  return [theSum, message];
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+multiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
