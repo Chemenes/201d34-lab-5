d8fb11447d12f3c49288e044261ef80fd0daa3bf
Christopher <conrad317@gmail.com>
2018-04-14 23:28:52 -0700

    problem 4 solved ls git status cd cd .. ~ ls -a
    status
    ls

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(a, b) { //eslint-disable-line
-//   var theAnswer = a + b;
-//   return [theAnswer, 'The sum of ' + a + ' and ' + b + ' is ' + theAnswer + '.'];
-// }
+function sum(a, b) { //eslint-disable-line
+  var theAnswer = a + b;
+  return [theAnswer, 'The sum of ' + a + ' and ' + b + ' is ' + theAnswer + '.'];
+}
 // console.log("The sum of 4 and 7 is 11.")
 // console.log(sum(4,7)[1])
-// // Here is the test for sum(); uncomment it to run it
+// Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -28,12 +28,12 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function multiply(a, b) { //eslint-disable-line
-//   var theProduct = a * b;
-//   var theMessage = 'The product of ' 
-//   + a + ' and ' + b + ' is ' + theProduct + '.';
-//   return [theProduct, theMessage];
-// }
+function multiply(a, b) { //eslint-disable-line
+  var theProduct = a * b;
+  var theMessage = 'The product of ' 
+  + a + ' and ' + b + ' is ' + theProduct + '.';
+  return [theProduct, theMessage];
+}
 // console.log("The product of 5 and 9 is 45")
 // console.log(multiply(5,9))
 
@@ -55,14 +55,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  var sumThreeNumber = sum(a, b)[0];
-  var sumC = sum(sumThreeNumber, c)[0];
-  var mulThreeNumber = multiply(a, b)[0];
-  var mulC = multiply(mulThreeNumber, c)[0];
-  return [sumC, mulC, a + ' and ' + b + ' and ' + c + ' sum to ' + sumC + '.', 'The product of' + a +  ' and '  + b +  ' and ' + c + ' is ' + mulC + '.'];
+  var threeNumber = sum(a, sum(b,c)[0])[0];
+
+  var oneSum = sum(a, b)[0]; 
+  var twoSum = sum(c, oneSum)[0];
+
+  var multiplyNum = multiply(a, multiply(b,c)[0])[0];
+
+  var returnOne = a + ' and ' + b + ' and ' + c + ' sum to ' + threeNumber + '.';
+  var returnTwo = 'The product of' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyNum + '.';
+ 
+  // return [threeNumber, multiplyNum, oneSum, twoSum];
 }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
+// // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -70,6 +76,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 /////////////////////////////////////
 /* Problem 4
 Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
@@ -81,12 +88,14 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+var theSum = sum(sumArr[0], sum(sumArr[1], sumArr[2]) [0]) [0];
+var message = sumArr + ' was passed in as an array of numbers, and '  + theSum + ' is their sum' + '.';
+return [theSum, message];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+sumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
